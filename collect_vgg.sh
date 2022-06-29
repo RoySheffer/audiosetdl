f1598f771926b910c95eae83a37b1f00d476fd1b
Roy Sheffer <roysheffer7@gmail.com>
2022-06-30 00:16:55 +0300

    adding my changes

--- collect_vgg.sh
+++ collect_vgg.sh
@@ -7,4 +7,5 @@ python $CWD/download_audioset.py /cs/dataset/Download/adiyoss/sheffer \
 --ffprobe /cs/labs/adiyoss/roysheffer/audiosetdl/ffmpeg-5.0.1-amd64-static/ffprobe \
 --audio-sample-rate 16000 \
 --audio-format wav \
+--num-workers 10 \
 --dataPath /cs/labs/adiyoss/roysheffer/audiosetdl/vggsound.csv
