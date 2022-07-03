f15d5a5e3b3c5d7f83467b2cb19f2c75fee6ccfb
Roy Sheffer <roysheffer7@gmail.com>
2022-07-03 22:57:28 +0300

    adding my changes

--- flac2wav.py
+++ flac2wav.py
@@ -1,6 +1,7 @@
 import os, glob
 from pydub import AudioSegment
 from pathlib import PurePath
+import traceback as tb
 
 # dataDir = r"C:\Roy\Msc\audiosetdl\vggsound"
 dataDir = r"/cs/dataset/Download/adiyoss/sheffer/vggsound"
@@ -16,5 +17,14 @@ if not os.path.exists(resultsDir):
 for flac in flacs:
     filename = os.path.basename(flac).split('.')[0]
     file_path = PurePath(flac)
-    flac_tmp_audio_data = AudioSegment.from_file(file_path, file_path.suffix[1:])
-    flac_tmp_audio_data.export(os.path.join(resultsDir, f'{filename}.wav'), format="wav")
+    wav_name = os.path.join(resultsDir, f'{filename}.wav')
+    if os.path.exists(wav_name):
+        print(f"{wav_name} Already exist")
+        continue
+    try:
+        flac_tmp_audio_data = AudioSegment.from_file(file_path, file_path.suffix[1:])
+        flac_tmp_audio_data.exportwav_name(wav_name, format="wav")
+    except Exception as e:
+        err_msg = '{} Error while processing video: {}; {} {}'.format(file_path, e, tb.format_exc(), "\n-----------------------------------------------------------------------\n")
+        print(err_msg)
+        continue
