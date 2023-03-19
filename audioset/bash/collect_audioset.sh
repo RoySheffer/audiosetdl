024b0395ea417a6acc29f2322030b480b391ac9a
Roy Sheffer <roysheffer7@gmail.com>
2023-03-19 18:35:25 +0200

    adding AC

--- audioset/bash/collect_audioset.sh
+++ audioset/bash/collect_audioset.sh
@@ -2,10 +2,10 @@
 
 CWD=$(pwd)
 
-python $CWD/download_audioset.py /cs/dataset/Download/adiyoss/sheffer/audiocaps \
+python $CWD/download_audioset.py /cs/dataset/Download/adiyoss/sheffer/AudioSet \
 --ffmpeg /cs/labs/adiyoss/roysheffer/audiosetdl/ffmpeg-5.0.1-amd64-static/ffmpeg \
 --ffprobe /cs/labs/adiyoss/roysheffer/audiosetdl/ffmpeg-5.0.1-amd64-static/ffprobe \
 --audio-sample-rate 16000 \
 --audio-format wav \
---num-workers 10 \
+--num-workers 20 \
 --dataPath /cs/labs/adiyoss/roysheffer/audiosetdl/audioset/unbalanced_train_segments.csv
