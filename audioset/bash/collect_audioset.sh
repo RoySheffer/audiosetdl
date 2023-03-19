c27cde0db15e5b98233891fff80e25f49abcbee2
Roy Sheffer <roysheffer7@gmail.com>
2023-03-19 20:08:59 +0200

    adding AC

--- audioset/bash/collect_audioset.sh
+++ audioset/bash/collect_audioset.sh
@@ -2,7 +2,7 @@
 
 CWD=$(pwd)
 
-python $CWD/download_audioset.py /cs/dataset/Download/adiyoss/sheffer/AudioSet \
+python $CWD/download_audioset.py /cs/dataset/Download/adiyoss/sheffer/passt/AudioSet \
 --ffmpeg /cs/labs/adiyoss/roysheffer/audiosetdl/ffmpeg-5.0.1-amd64-static/ffmpeg \
 --ffprobe /cs/labs/adiyoss/roysheffer/audiosetdl/ffmpeg-5.0.1-amd64-static/ffprobe \
 --audio-sample-rate 16000 \
