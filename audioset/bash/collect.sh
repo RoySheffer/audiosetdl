7381b8634fbb0c91993bdc1a96bc0e5abc087c23
Roy Sheffer <roysheffer7@gmail.com>
2023-04-15 14:40:14 +0300

    adding AC

--- audioset/bash/collect.sh
+++ audioset/bash/collect.sh
@@ -2,4 +2,4 @@
 
 CWD=$(pwd)
 
-sbatch  --time=7-0 --mem=10g -c6 --job-name=AS --wrap "source /cs/labs/adiyoss/roysheffer/audiosetdl/activate; bash /cs/labs/adiyoss/roysheffer/audiosetdl/audioset/bash/collect_audioset.sh"
+sbatch  --time=7-0 --mem=10g -c6 --mem-per-cpu=20g --job-name=AS --wrap "source /cs/labs/adiyoss/roysheffer/audiosetdl/activate; bash /cs/labs/adiyoss/roysheffer/audiosetdl/audioset/bash/collect_audioset.sh"
